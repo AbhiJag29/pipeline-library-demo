f5c99addc4162d60746d840a516dc438f98ac03b
AbhiJag29 <87749240+AbhiJag29@users.noreply.github.com>
2021-08-16 15:56:38 +0530

    Create readFile.groovy

---
+++ readFile.groovy
@@ -0,0 +1,11 @@
+def call(String str ) 
+{	
+	def filePath = readFile "${str}"                   
+	def lines = filePath.readLines()
+	def linesbyline = filePath.readLines() 
+     
+	for (line in linesbyline) 
+  {
+    println "$line"
+  }  
+}
